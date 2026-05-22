5a36d660f7be657af3aac3274fa66c370c00de02
shanilgd <shanilgd@gmail.com>
2026-05-22 11:59:50 +0530

    Delete preload.js

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    getVersion: () => ipcRenderer.invoke('get-app-version'),
-    checkForUpdates: () => ipcRenderer.invoke('check-for-updates'),
-    onUpdateStatus: (callback) => ipcRenderer.on('update-status', (event, ...args) => callback(...args))
-});
